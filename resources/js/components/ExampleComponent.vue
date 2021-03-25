19c59664e34bda3f5671b847d867e34b440a215a
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-03-25 10:46:40 +0800

    updates

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Hi! This is the Vue Component We Added!
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
